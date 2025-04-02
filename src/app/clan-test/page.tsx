b037b503c784bfc44f09877b9fc3335b9c55a11c
johovenn <jonathan18102003@gmail.com>
2025-04-02 11:47:33 +0700

    adjusted text glow on various screen sizes

--- src/app/clan-test/page.tsx
+++ src/app/clan-test/page.tsx
@@ -1,11 +1,16 @@
 export default function ClanTestPage(){
+    // const questions = [
+    //     {
+    //         "question": ""
+    //     },
+    // ]
     
-        return(
-            <div
-                className="h-screen w-screen flex items-center justify-evenly bg-cover bg-center" 
-                style={{ backgroundImage: "url('/background.png')" }}
-            >   
+    return(
+        <div
+            className="h-screen w-screen flex items-center justify-evenly bg-cover bg-center" 
+            style={{ backgroundImage: "url('/background.png')" }}
+        >   
 
-            </div>
-        )
+        </div>
+    )
 }
