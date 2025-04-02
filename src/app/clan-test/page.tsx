42a59388c7a7542cbc71b87d0923047584b5eb9e
johovenn <jonathan18102003@gmail.com>
2025-04-02 10:24:40 +0700

    add landing page

---
+++ src/app/clan-test/page.tsx
@@ -0,0 +1,11 @@
+export default function ClanTestPage(){
+    
+        return(
+            <div
+                className="h-screen w-screen flex items-center justify-evenly bg-cover bg-center" 
+                style={{ backgroundImage: "url('/background.png')" }}
+            >   
+
+            </div>
+        )
+}
---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,28 @@
+import { PressStart2P } from "@/app/page";
+import { ReactNode } from "react";
+
+interface CustomButtonProps {
+    children: ReactNode | string;
+    onClick?: () => void;
+    className?: string;
+    disabled?: boolean;
+}
+
+export default function CustomButton(props: CustomButtonProps) {
+    return (
+        <button
+            className={`
+                ${PressStart2P.className} px-7 py-3 bg-gradient-to-tr from-purple-600 via-purple-800 to-purple-700
+                hover:from-purple-700 hover:via-purple-900 hover:to-purple-800
+                transition-all duration-3000 ease-in-out outline-2 outline-white text-white rounded-md text-sm
+                ${props.className} ${props.disabled ? "opacity-50 cursor-not-allowed" : ""}
+                shadow-[0_0_20px_2px_rgba(128,0,128,0.7)]  shadow-purple-600
+                hover:shadow-[0_0_30px_5px_rgba(128,0,128,0.7)] 
+            `}
+            onClick={props.onClick}
+            disabled={props.disabled}
+        >
+            {props.children}
+        </button>
+    );
+}
