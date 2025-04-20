51ded8a4d97aab0720fce61b53a7ab787532c9e7
johovenn <110096623+Johovenn@users.noreply.github.com>
2025-04-20 14:23:55 +0700

    change question box text

--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,5 +1,6 @@
-import { JetBrains_Mono } from "next/font/google";
-const JetBrains = JetBrains_Mono({ subsets: ['latin'], weight: ["400", "700", "800"] });
+import { Montserrat } from "next/font/google";
+
+const monstserrat = Montserrat({ subsets: ['latin'], weight: ["400", "700", "800"] });
 
 interface QuestionBoxProps {
   question: string;
@@ -11,7 +12,7 @@ export default function QuestionBox(props: QuestionBoxProps) {
             className="relative w-[80%] py-8 md:py-12 flex items-center justify-center text-center overflow-hidden mt-2 bg-gradient-to-b from-[#34036c] to-[#4535cd] border-white border-4"
         >
             <div className="relative z-10 px-7">
-                <p className={`${JetBrains.className} text-white text-sm md:text-md lg:text-lg xl:text-3xl font-bold text-shadow-amber-200 text-shadow px-5`}>
+                <p className={`${monstserrat.className} text-white text-sm md:text-md lg:text-lg xl:text-3xl font-bold text-shadow-amber-200 text-shadow px-5`}>
                     {props.question.toUpperCase()}
                 </p>
             </div>
